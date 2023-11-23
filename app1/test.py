93b18b80d7af3294d20fc2934becec2d1194d866
kinanti18 <anastasia.putri162001@gmail.com>
2023-11-23 08:03:22 +0700

    Create basic GUI without interactions, day 16

---
+++ app1/test.py
@@ -0,0 +1,9 @@
+def greet():
+    message = "hello"
+    new_message = message.capitalize()
+    print("Hey hey")
+    return new_message
+
+user_entry = input("What greeting do you want?")
+greeting = greet(user_entry)
+print(greeting)
